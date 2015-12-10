ecb984ef83c242dbb5e6fa1ea342ad89fae577f7
Karl Parakenings <karl@bluebatgames.com>
2015-12-10 15:48:01 -0800

    idk wtf

--- src/start.cpp
+++ src/start.cpp
@@ -1,16 +1,14 @@
+#include <BearLibTerminal.h>
 #include <cstdio>
-#include "libtcod.hpp"
-
 // Hide the console window
-#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 
 int main()
 {
-	TCODConsole::initRoot(80, 50, "C++ libtcod tutorial");
-	TCODConsole::root->printEx(40, 25, TCOD_BKGND_NONE, TCOD_CENTER, "Hello world");
-	TCODConsole::flush();
-	TCODConsole::waitForKeypress(true);
-
+	terminal_open();
+	terminal_print(1, 1, "Hello, world!");
+	terminal_refresh();
+	while (terminal_read() != TK_CLOSE);
+	terminal_close();
 	return 0;
 
 }
